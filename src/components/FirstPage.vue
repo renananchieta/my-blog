ea9a0e42ac08fa4a497a7b06559bd6a9e9f17370
Renan Anchieta <renangap18@gmail.com>
2023-07-05 00:39:20 -0300

    retornar a posição das fotos

--- src/components/FirstPage.vue
+++ src/components/FirstPage.vue
@@ -1,18 +1,6 @@
 <template>
   <v-container class="fill-height">
     <v-responsive class="align-center text-center fill-height">
-      <h1 class="text-h2 font-weight-bold">Renan Anchieta</h1>
-      
-      <div class="text-title-2 mb-n1">Cursando Análise e Desenvolvimento de Sistemas na Universidade da Amazônia - UNAMA.  </div>
-      <div class="text-title-2 mb-n1">
-        Programador Júnior na Polícia Civil do Estado do Pará. 
-      </div>
-      <div class="text-title-2 mb-n1">
-        Experiência em sistemas de recursos humanos, financeiro e administrativo.
-      </div>
-      <div class="text-title-2 mb-n1">
-        Competências: PHP, Laravel, JavaScript, Vue.Js, MySQL e PostgreSQL.
-      </div>
       <v-row>
         <v-col  cols="6"
         sm="4">
@@ -28,6 +16,18 @@
         </v-col>
       </v-row>
       <br>
+      <h1 class="text-h2 font-weight-bold">Renan Anchieta</h1>
+      
+      <div class="text-title-2 mb-n1">Cursando Análise e Desenvolvimento de Sistemas na Universidade da Amazônia - UNAMA.  </div>
+      <div class="text-title-2 mb-n1">
+        Programador Júnior na Polícia Civil do Estado do Pará. 
+      </div>
+      <div class="text-title-2 mb-n1">
+        Experiência em sistemas de recursos humanos, financeiro e administrativo.
+      </div>
+      <div class="text-title-2 mb-n1">
+        Competências: PHP, Laravel, JavaScript, Vue.Js, MySQL e PostgreSQL.
+      </div>
       <div class="py-14" />
     </v-responsive>
   </v-container>
